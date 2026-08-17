68f1faf9b4f4f79adaff558c00a322e34536367e
あなたの名前 <yaopingdongdao@gmail.com>
2026-08-17 12:08:50 +0900

    ci: GitHub Actions CI/CD追加

---
+++ laravel-docker-app/myapp/src/utils/price.js
@@ -0,0 +1,3 @@
+export function calcTaxIncluded(price, taxRate = 0.1){
+    return Math.floor(price * (1 + taxRate))
+}
---
+++ laravel-docker-app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-week9refactoring',
+        'version' => 'dev-week9refactoring',
+        'reference' => '38117b7a503ed43bb97c5deac3ffde6199cbc4d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-week9refactoring',
+            'version' => 'dev-week9refactoring',
+            'reference' => '38117b7a503ed43bb97c5deac3ffde6199cbc4d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v21.1.1',
+            'version' => '21.1.1.0',
+            'reference' => '5f68d5a285d206a757ebbd9bf0e037759a61c379',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
