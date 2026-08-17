02c148fd66b33bd95b328db831cde8161aff7543
あなたの名前 <yaopingdongdao@gmail.com>
2026-08-17 13:25:13 +0900

    chore: 意図しない重複プロジェクトlaravel-docker-appを削除、myapp内の重複ワークフローも整理

--- laravel-docker-app/myapp/resources/views/emails/post-created.blade.php
+++
@@ -1,2 +0,0 @@
-// resources/views/emails/post-created.blade.php
-<p>新しい投稿が作成されました。</p>
--- laravel-docker-app/myapp/src/utils/price.js
+++
@@ -1,3 +0,0 @@
-export function calcTaxIncluded(price, taxRate = 0.1){
-    return Math.floor(price * (1 + taxRate))
-}
